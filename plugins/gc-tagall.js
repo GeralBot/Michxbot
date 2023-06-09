d4245d67abc59e7b776263dd46e4d297e403140c
GeralBot <133059729+GeralBot@users.noreply.github.com>
2023-06-08 23:25:01 -0500

    Geral Bot

--- plugins/gc-tagall.js
+++ plugins/gc-tagall.js
@@ -3,12 +3,12 @@ if (!(isAdmin || isOwner)) {
 global.dfail('admin', m, conn)
 throw false
 }
-let pesan = args.join` `
-let oi = `《 @𝐠𝐞𝐫𝐚𝐥_𝐯𝐞𝐧𝐭𝐚𝐬𝟏𝟖 》: ${pesan}`
-let teks = `《𝐑𝐞𝐯𝐢𝐯𝐚𝐧 𝐁𝐨𝐭𝐬 - 𝐆𝐞𝐫𝐚𝐥 𝐁𝐨𝐭》\n\n❏ ${oi}\n\n❏ 《 𝐄𝐭𝐢𝐪𝐮𝐞𝐭𝐚𝐬 》:\n`
+let pesan = args.join` Bot creado por Wa.me/51935763050 prohibo la venta del bot `
+let oi = `《 @𝐱.𝐦𝐢𝐜𝐡𝐛𝐦.𝐱 》: ${pesan}`
+let teks = `《𝐑𝐞𝐯𝐢𝐯𝐚𝐧 𝐁𝐨𝐭𝐬 - 𝐌𝐢𝐜𝐡𝐱 𝐁𝐨𝐭》\n\n❏ ${oi}\n\n❏ 《 𝐄𝐭𝐢𝐪𝐮𝐞𝐭𝐚𝐬 》:\n`
 for (let mem of participants) {
 teks += `✼🩵💎 @${mem.id.split('@')[0]}\n`}
-teks += `➥ 𝐆𝐞𝐫𝐚𝐥 𝐁𝐨𝐭 `
+teks += `➥ 𝐌𝐢𝐜𝐡𝐱 𝐁𝐨𝐭 `
 conn.sendMessage(m.chat, { text: teks, mentions: participants.map(a => a.id) }, )
 }
 handler.help = ['tagall <mesaje>','invocar <mesaje>']
